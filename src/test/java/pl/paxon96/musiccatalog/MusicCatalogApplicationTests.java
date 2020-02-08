7f1d0f5ccdb45c977f1c2fdbd83f0ef79450d265
Paxon96 <bwloczyk@gmail.com>
2020-02-08 11:51:27 +0100

    initial commit

---
+++ src/test/java/pl/paxon96/musiccatalog/MusicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.paxon96.musiccatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
